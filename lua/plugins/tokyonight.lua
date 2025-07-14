0c857a236cb5849007eea83a1ce9ecbf982146bf
Max Jeschek <max@jeschek.dev>
2025-07-14 18:04:26 +0200

    update tokyonight config: enable caching, set style and light_style to "night", adjust day_brightness, enable dim_inactive and lualine_bold, disable terminal_colors, add empty plugins table, add custom on_colors and on_highlights functions (Visual highlight group), keep transparent background, keep sidebar/floats transparent

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -4,11 +4,23 @@ return {
 	priority = 1000,
 	config = function()
 		require("tokyonight").setup({
+			on_colors = function(colors) end,
+			style = "night",
+			cache = true,
+			day_brightness = 0.3,
+			light_style = "night",
+			dim_inactive = true,
+			lualine_bold = true,
+			plugins = {},
+			terminal_colors = false,
 			transparent = true,
 			styles = {
 				sidebars = "transparent",
 				floats = "transparent",
 			},
+			on_highlights = function(hl, c)
+				hl.Visual = { bg = "#3d59a1", fg = "NONE" }
+			end,
 		})
 
 		vim.cmd([[ colorscheme tokyonight-night ]])
