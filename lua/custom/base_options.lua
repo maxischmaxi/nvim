cfa5cb27457604fbd8c544f684f8095f403518d1
Max Jeschek <max@jeschek.dev>
2025-07-14 17:59:09 +0200

    add function to detect Prettier config files and set tab width options accordingly, triggering on BufEnter autocmd

--- lua/custom/base_options.lua
+++ lua/custom/base_options.lua
@@ -29,6 +29,42 @@ local vimOptions = {
 	smartindent = false,
 }
 
+local function set_tabs_from_prettier()
+	local config_files = {
+		".prettierrc",
+		".prettierrc.json",
+		".prettierrc.js",
+		".prettierrc.cjs",
+		".prettierrc.yaml",
+		".prettierrc.yml",
+		".prettierrc.toml",
+	}
+
+	local cwd = vim.fn.getcwd()
+
+	for _, file in ipairs(config_files) do
+		local path = cwd .. "/" .. file
+		if vim.fn.filereadable(path) == 1 then
+			for line in io.lines(path) do
+				local tabwidth = line:match([["tabWidth"%s*:%s*(%d+)]])
+				if tabwidth then
+					local width = tonumber(tabwidth)
+					vim.opt.tabstop = width
+					vim.opt.shiftwidth = width
+					vim.opt.softtabstop = width
+					break
+				end
+			end
+			break
+		end
+	end
+end
+
+vim.api.nvim_create_autocmd("BufEnter", {
+	pattern = "*",
+	callback = set_tabs_from_prettier,
+})
+
 local windowOptions = {
 	number = true,
 	signcolumn = "yes",
